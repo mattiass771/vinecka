67cb9b8f7bfb676cdb11cfca0f50d79428ef0bc0
Matus Zilinsky <matus.zilinsky@accenture.com>
2021-03-02 21:31:55 +0100

    delete email from newsletters method

--- db/newsEmailsDb.js
+++ db/newsEmailsDb.js
@@ -11,23 +11,36 @@ const mailsSchema = new Schema({
 const Mails = mongoose.model("Mails", mailsSchema);
 
 router.route("/").get((req, res) => {
-    Mails.findOne()
+    Mails.find()
       .then((mails) => res.json(mails))
       .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
 router.route("/add").post((req, res) => {
     const { name, email } = req.body;
-
     const addMails = new Mails({
-    name,
-    email
+        name,
+        email
     });
     addMails
     .save()
     .then(() => res.json(`Mail remembered!`))
     .catch((err) => res.status(400).json(`Error: ${err}`));
-    
+});
+
+router.route("/delete-from-newsletter/").post((req, res) => {
+    const { email } = req.body;
+    Mails.find((err, mailsFound) => {
+      if (err) return console.log(err.data);
+      mailsFound = mailsFound.filter(
+        (val) => val.email !== email
+      );
+      mailsFound
+        .save()
+        .then(() =>
+          res.json(`Mail: ${email} was removed from your mails.`)
+        );
+    });
   });
 
 module.exports = {
