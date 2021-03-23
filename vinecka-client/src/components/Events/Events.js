cf6179e6227c682bb57fbd6e3d308eddf2a37cb8
Matus Zilinsky <matus.zilinsky@accenture.com>
2021-03-23 16:53:48 +0100

    added masonry for events

--- vinecka-client/src/components/Events/Events.js
+++ vinecka-client/src/components/Events/Events.js
@@ -64,59 +64,69 @@ export default ({isOwner}) => {
     }
 
     const ShowEvents = () => {
-        return eventsData.map(event => {
+        const output = eventsData.map((event, i) => {
             const { _id, name, link, description, imageLink, when, where, until } = event
             return (
-                <Col key={_id} style={{maxWidth:'800px'}} className="mt-4" md={12} lg={6}>
-                    <Card className="h-100" id={_id}>
-                        {isOwner &&
-                            <Button
-                                onClick={() => deleteCard(_id)}
-                                style={{
-                                    width: "40px",
-                                    height: "40px",
-                                    marginBottom: "-40px",
-                                    zIndex: "+5"
-                                }}
-                                variant="outline-danger"
-                                >
-                                <MdDelete style={{ fontSize: "150%", margin: "0 0 15px -5px" }} />
-                            </Button>}
-                        {isOwner &&
+                <Card key={_id} className="h-100 mt-4" id={_id}>
+                    {isOwner &&
                         <Button
-                            onClick={(e) => handleEditing(e)}
+                            onClick={() => deleteCard(_id)}
                             style={{
                                 width: "40px",
                                 height: "40px",
                                 marginBottom: "-40px",
-                                marginLeft: "40px",
                                 zIndex: "+5"
                             }}
-                            variant="outline-warning"
+                            variant="outline-danger"
                             >
-                            <MdEdit style={{ fontSize: "150%", margin: "0 0 15px -5px" }} />
-                            </Button>}
-                            
-                        {editing &&
-                            <EditEvent refresh={refresh} setRefresh={setRefresh} setEventPopup={setEditing} eventPopup={editing[_id]} eventData={event} />
-                        }
-                        <Card.Img variant="top" style={{ height: '350px', width: '100%', objectFit: 'cover' }} src={getImage(imageLink) ? getImage(imageLink) : imageLink} />
-                        <Card.Body>
-                            <Card.Title>{name}<Button onClick={() => handleSubmit(link)} size="sm" variant="dark" style={{float: 'right'}}>Viac info</Button></Card.Title>
-                            <Card.Text dangerouslySetInnerHTML={{__html: description}}></Card.Text>
-                        </Card.Body>
-                        {(when || where) &&
-                        <Card.Footer>
-                            <small className="text-muted">
-                                <MdDateRange style={{fontSize: '130%'}} /> {when}{until && ` - ${until}`}
-                                <br /> 
-                                <a className="link-no-deco" onClick={() => setShowMapPopup(where)}><MdLocationOn style={{fontSize: '130%'}} /> {where}</a>
-                            </small>
-                        </Card.Footer>}
-                    </Card>
-                </Col>
+                            <MdDelete style={{ fontSize: "150%", margin: "0 0 15px -5px" }} />
+                        </Button>}
+                    {isOwner &&
+                    <Button
+                        onClick={(e) => handleEditing(e)}
+                        style={{
+                            width: "40px",
+                            height: "40px",
+                            marginBottom: "-40px",
+                            marginLeft: "40px",
+                            zIndex: "+5"
+                        }}
+                        variant="outline-warning"
+                        >
+                        <MdEdit style={{ fontSize: "150%", margin: "0 0 15px -5px" }} />
+                        </Button>}
+                        
+                    {editing &&
+                        <EditEvent refresh={refresh} setRefresh={setRefresh} setEventPopup={setEditing} eventPopup={editing[_id]} eventData={event} />
+                    }
+                    <Card.Img variant="top" style={{ height: '350px', width: '100%', objectFit: 'cover' }} src={getImage(imageLink) ? getImage(imageLink) : imageLink} />
+                    <Card.Body>
+                        <Card.Title>{name}<Button onClick={() => handleSubmit(link)} size="sm" variant="dark" style={{float: 'right'}}>Viac info</Button></Card.Title>
+                        <Card.Text dangerouslySetInnerHTML={{__html: description}}></Card.Text>
+                    </Card.Body>
+                    {(when || where) &&
+                    <Card.Footer>
+                        <small className="text-muted">
+                            <MdDateRange style={{fontSize: '130%'}} /> {when}{until && ` - ${until}`}
+                            <br /> 
+                            <a className="link-no-deco" onClick={() => setShowMapPopup(where)}><MdLocationOn style={{fontSize: '130%'}} /> {where}</a>
+                        </small>
+                    </Card.Footer>}
+                </Card>
             )
         })
+        const colRight = output.filter((card,i) => i%2 === 0)
+        const colLeft = output.filter((card,i) => i%2 !== 0)
+        return (
+            <Row className="justify-content-center justify-content-md-start" >
+                <Col className={`mt-4 h-100`} md={12} lg={6}>
+                    {colRight}
+                </Col>
+                <Col className={`mt-4 h-100`} md={12} lg={6}>
+                    {colLeft}
+                </Col>
+            </Row>
+        )
     }
 
     return (
@@ -131,11 +141,7 @@ export default ({isOwner}) => {
             <Row className="justify-content-center" >
                 <Button className="mt-4" variant="dark" onClick={() => setEventPopup(true)}>Pridat Event</Button>
             </Row>}
-            <Row className="justify-content-center justify-content-md-start" >
-                {/* <CardColumns> */}
-                    <ShowEvents />
-                {/* </CardColumns> */}
-            </Row>
+            <ShowEvents />
         </Container>
     )
 }
